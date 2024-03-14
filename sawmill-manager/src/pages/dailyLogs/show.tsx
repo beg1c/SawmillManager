62c05b484e7e9a83f6f59289038fc4d7e93a492c
markobegic97@gmail.com <72312119+beg1c@users.noreply.github.com>
2024-03-14 09:39:17 +0100

    F-B-: Daily log delete

--- sawmill-manager/src/pages/dailyLogs/show.tsx
+++ sawmill-manager/src/pages/dailyLogs/show.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Box, Grid, Paper, Stack, Typography } from "@mui/material";
-import { HttpError, IResourceComponentsProps, useCan, useShow } from "@refinedev/core";
+import { HttpError, IResourceComponentsProps, useCan, useNavigation, useShow } from "@refinedev/core";
 import { IDailyLog, IMaterialWQuantity, IProductWQuantity, IWasteWQuantity } from "../../interfaces/interfaces";
 import { useTranslation } from "react-i18next";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
@@ -8,7 +8,7 @@ import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React from "react";
 import { ForestOutlined, LocalGroceryStoreOutlined, RecyclingOutlined } from "@mui/icons-material";
-import { EditButton, List } from "@refinedev/mui";
+import { DeleteButton, EditButton, List } from "@refinedev/mui";
 import { AddProducts } from "../../components/dailyLog";
 import { useModalForm } from "@refinedev/react-hook-form";
 import { AddMaterials } from "../../components/dailyLog/addMaterials";
@@ -20,6 +20,7 @@ export const DailyLogShow: React.FC<IResourceComponentsProps> = () => {
     const { queryResult } = useShow<IDailyLog>({});
     const { data } = queryResult;
     const dailyLog = data?.data;
+    const { push } = useNavigation();
 
     const productsDrawerFormProps = useModalForm<
         IDailyLog,
@@ -217,18 +218,31 @@ export const DailyLogShow: React.FC<IResourceComponentsProps> = () => {
                             <Typography variant="h6">
                                 {dailyLog?.sawmill.name}
                             </Typography>
-                            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                <DatePicker
-                                    readOnly
-                                    value={dailyLog?.date && isValid(parseISO(dailyLog?.date.toString())) ? 
-                                        parseISO(dailyLog?.date.toString()) : null}
-                                    slotProps={{
-                                        textField: { 
-                                            size: 'small',
-                                        } 
-                                    }} 
+                            <Box 
+                                display='flex'
+                                alignItems='center'
+                            >  
+                                <DeleteButton 
+                                    hideText={true}
+                                    sx={{ marginRight: 1 }}
+                                    size="large"
+                                    onSuccess={() => {  
+                                        push("/dailylogs");
+                                    }}  
                                 />
-                            </LocalizationProvider>
+                                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                    <DatePicker
+                                        readOnly
+                                        value={dailyLog?.date && isValid(parseISO(dailyLog?.date.toString())) ? 
+                                            parseISO(dailyLog?.date.toString()) : null}
+                                        slotProps={{
+                                            textField: { 
+                                                size: 'small',
+                                            } 
+                                        }} 
+                                    />
+                                </LocalizationProvider>
+                            </Box>
                         </Paper>
                     </Grid>
                     <Grid item xs={12} lg={4}>
